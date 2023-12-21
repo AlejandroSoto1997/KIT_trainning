bdd5fb9b1d1e454431d561ba883871ee5dcd593c
Alejandro Soto <asm19971997@gmail.com>
2023-12-21 13:02:44 -0400

    added multiple system

---
+++ relaxed struc/single systems/plots_ad.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+# Directorio base
+base_directory = '/Users/alejandrosoto/Downloads/KIT/Tools/oxDNA/Task 1 Francesco/Y shape /relaxing diff cluster '  # Reemplaza con la ruta de tu directorio base
+
+# Lista de subcarpetas
+subfolders = [
+    'salt_0.15_58C', 'salt_0.1_58C', 'salt_0.2_58C',
+    'salt_0.15_55C', 'salt_0.1_55C', 'salt_0.2_55C',
+    'salt_0.15_60C', 'salt_0.1_60C', 'salt_0.2_60C'
+]
+
+# Diccionario para almacenar las medianas
+median_energy_dict = {}
+
+# Recorre las subcarpetas y procesa los archivos
+for subfolder in subfolders:
+    folder_path = os.path.join(base_directory, subfolder, 'y_shape', 'duplex+se')
+    file_path = os.path.join(folder_path, 'energy_file_md.txt')
+
+    if os.path.exists(file_path):
+        # Lee el archivo de energía con pandas
+        df = pd.read_csv(file_path, sep='\s+', names=['Time', 'Energy1', 'Energy2', 'Energy3'])
+
+        # Calcula y almacena la mediana de la tercera columna
+        median_energy = df['Energy3'].median()
+        median_energy_dict[subfolder] = median_energy
+        print(f'Median Total Energy for {subfolder}: {median_energy}')
+    else:
+        print(f'File not found: {file_path}')
+
+# Crea un DataFrame a partir del diccionario de medianas
+median_energy_df = pd.DataFrame(list(median_energy_dict.items()), columns=['Subfolder', 'MedianEnergy'])
+
+# Separa la columna "Subfolder" en tres columnas usando el guion bajo como separador
+median_energy_df[['Salt', 'salt_conc', 'temp']] = median_energy_df['Subfolder'].str.split('_', expand=True)
+
+# Elimina la letra "C" y convierte la columna "temp" a número
+median_energy_df['temp'] = median_energy_df['temp'].str.rstrip('C').astype(float)
+# Convierte la columna "salt_conc" a número
+median_energy_df['salt_conc'] = median_energy_df['salt_conc'].astype(float)
+
+# Ordena el DataFrame por temperatura y concentración de sal
+median_energy_df.sort_values(['temp', 'salt_conc'], inplace=True)
+
+# Crea dos gráficos subplots
+fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+
+# Gráfico 1: Salt Concentration vs Temperature
+for salt_conc, group_df in median_energy_df.groupby('salt_conc'):
+    axes[0].plot(group_df['temp'], group_df['MedianEnergy'], marker='o', label=f'Salt conc. = {salt_conc} mM')
+
+axes[0].set_title('Representative total energy (eq) vs temperature')
+axes[0].set_xlabel('Temperature (°C)')
+axes[0].set_ylabel('Representative total energy (eq)')
+axes[0].legend()
+axes[0].grid(True)
+
+# Gráfico 2: Temperature vs Salt Concentration
+for temp, group_df in median_energy_df.groupby('temp'):
+    axes[1].plot(group_df['salt_conc'], group_df['MedianEnergy'], marker='o', label=f'Temp. = {temp}°C')
+
+axes[1].set_title('Representative total energy (eq) vs salt concentration')
+axes[1].set_xlabel('Salt concentration (mM)')
+axes[1].set_ylabel('Representative total energy (eq)')
+axes[1].legend()
+axes[1].grid(True)
+
+# Ajusta el paso en el eje x de la segunda gráfica
+axes[1].xaxis.set_major_locator(plt.MultipleLocator(0.05))
+
+plt.show()
---
+++ relaxed struc/y shape + linker/plot_md.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 17 20:01:22 2023
+
+@author: alejandrosoto
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Lee el archivo de energía con pandas
+file_path = 'energy_file_md.txt'  # Reemplaza con el nombre real de tu archivo
+df = pd.read_csv(file_path, sep='\s+', names=['Time', 'Energy1', 'Energy2', 'Energy3'])
+
+# Grafica las tres energías en función del tiempo en escala logarítmica
+plt.figure(figsize=(10, 6)) 
+plt.plot(df['Time'], np.log(-df['Energy1']), label='Potential energy')
+plt.plot(df['Time'], np.log(df['Energy2']), label='Kinetic energy')
+plt.plot(df['Time'], np.log(-df['Energy3']), label='Total energy')
+
+# Configura el gráfico
+plt.title('Logaritmic profiles of energies')
+plt.xlabel('Time')
+plt.ylabel('Log(Energy)')
+plt.legend()
+plt.grid(True)
+plt.show()
